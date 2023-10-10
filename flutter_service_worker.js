6c3250d913d2cc733cd0f51de4a7046789076a7e
Kanwarpreet Singh <preetkewl@gmail.com>
2023-10-10 16:35:03 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "edd1e54a67e13fe601da483c8d08e859",
-"index.html": "e929d5b42617a6d3dbcc4a977f235371",
-"/": "e929d5b42617a6d3dbcc4a977f235371",
-"main.dart.js": "22885b190db18f5bbf7c3350b884a6ff",
+"index.html": "dae22ee806f59e60b8ff7f37a800bc8e",
+"/": "dae22ee806f59e60b8ff7f37a800bc8e",
+"main.dart.js": "7d511741baeb3ac528cebacf72407a09",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
